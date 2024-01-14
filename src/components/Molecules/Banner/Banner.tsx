492ac38ea28b033342cc3618aadf4e762beb3185
Vítor O. <Caulicons@hotmail.com>
2024-01-14 14:27:24 -0300

    fix(component): banner text alignment.

--- src/components/Molecules/Banner/Banner.tsx
+++ src/components/Molecules/Banner/Banner.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { motion } from 'framer-motion';
 import cn from '../../../../utils/cn';
 import BannerImage from './BannerImage';
@@ -19,10 +19,10 @@ export default function Banner({
       {animation ? (
         <motion.div
           initial={{ opacity: 0, top: 0 }}
-          animate={{ opacity: 1, top: '45%' }}
+          animate={{ opacity: 1, top: '50%' }}
           transition={{ duration: 1.8, ease: 'easeInOut' }}
           className={cn(
-            'absolute left-1/2 top-1/2 flex w-4/5 -translate-x-1/2 -translate-y-1/2 flex-col gap-8 text-center ',
+            'absolute left-1/2  flex w-4/5 -translate-x-1/2 -translate-y-1/2 flex-col gap-8 text-center ',
             className
           )}
         >
@@ -31,7 +31,7 @@ export default function Banner({
       ) : (
         <div
           className={cn(
-            'absolute left-1/2 top-1/2 flex w-4/5 -translate-x-1/2 -translate-y-1/2 flex-col gap-8 text-center',
+            'absolute left-1/2 top-[55%] flex w-4/5 -translate-x-1/2 -translate-y-1/2 flex-col gap-8 text-center',
             className
           )}
         >
